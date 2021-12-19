80ffe0e7e50061b9b24499265b6806082f0b99c9
Evolva <ber.fabien@gmail.com>
2021-12-19 01:20:25 +0100

    Add Day12

---
+++ AdventOfCode/AdventOfCode/Day12/Day12.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Day05
+{
+    public class Day12 : AdventCalendarSolver
+    {
+        public class CaveMap
+        {
+            public static CaveMap ReadFromFile(string[] input)
+            {
+                var nodeLinks = new Dictionary<string, HashSet<string>>();
+
+                foreach (var line in input)
+                {
+                    var split = line.Split('-');
+                    var from = split[0];
+                    var to = split[1];
+
+                    if (nodeLinks.TryGetValue(from, out var fromLinks))
+                    {
+                        fromLinks.Add(to);
+                    }
+                    else
+                    {
+                        nodeLinks[from] = new HashSet<string> { to };
+                    }
+
+                    if (nodeLinks.TryGetValue(to, out var toLinks))
+                    {
+                        toLinks.Add(from);
+                    }
+                    else
+                    {
+                        nodeLinks[to] = new HashSet<string> { from };
+                    }
+                }
+
+                return new CaveMap(nodeLinks);
+            }
+
+            private readonly Dictionary<string, HashSet<string>> nodeLinks;
+
+            private CaveMap(Dictionary<string, HashSet<string>> nodeLinks)
+            {
+                this.nodeLinks = nodeLinks;
+            }
+
+            public long CountPathsWithoutVisitingSmallCaveTwice()
+            {
+                var possiblePath = new HashSet<string>();
+                var pathQueue = new Queue<string>();
+                pathQueue.Enqueue("start");
+
+                while (pathQueue.Any())
+                {
+                    var currentPath = pathQueue.Dequeue();
+                    var visitedNodes = currentPath.Split(',');
+                    var lastNode = visitedNodes.Last();
+
+                    var possibleNextNodes = nodeLinks[lastNode];
+                    foreach(var possibleNextNode in possibleNextNodes)
+                    {
+                        if (possibleNextNode == "end")
+                        {
+                            //reached the end 
+                            possiblePath.Add($"{currentPath},end");
+                            continue;
+                        }
+                        
+                        if (IsSmallCave(possibleNextNode) && visitedNodes.Contains(possibleNextNode))
+                        {
+                            //small cave already visited
+                            continue;
+                        }
+
+                        pathQueue.Enqueue($"{currentPath},{possibleNextNode}");
+                    }
+                }
+
+                return possiblePath.Count;
+            }
+
+            public long CountPathVisitingASingleSmallCaveTwice()
+            {
+                var possiblePath = new HashSet<string>();
+                var pathQueue = new Queue<string>();
+                pathQueue.Enqueue("start");
+
+                while (pathQueue.Any())
+                {
+                    var currentPath = pathQueue.Dequeue();
+                    var visitedNodes = currentPath.Split(',');
+                    var lastNode = visitedNodes.Last();
+
+                    var possibleNextNodes = nodeLinks[lastNode];
+                    foreach (var possibleNextNode in possibleNextNodes)
+                    {
+                        if (possibleNextNode == "start")
+                        {
+                            //can't return to start
+                            continue;
+                        }
+
+                        if (possibleNextNode == "end")
+                        {
+                            //reached the end 
+                            possiblePath.Add($"{currentPath},end");
+                            continue;
+                        }
+
+                        if (IsSmallCave(possibleNextNode) && !CanVisitSmallCave(visitedNodes, possibleNextNode))
+                        {
+                            continue;
+                        }
+
+                        pathQueue.Enqueue($"{currentPath},{possibleNextNode}");
+                    }
+                }
+
+                return possiblePath.Count;
+            }
+
+            private bool IsSmallCave(string s) => s.All(c => 'a' <= c && c <= 'z');
+
+            private bool CanVisitSmallCave(string[] visitedNodes, string smallCave)
+            {
+                var visitedSmallCave = new HashSet<string>();
+                var aNodeHasBeenVisitedTwice = false;
+
+                foreach(var node in visitedNodes.Where(n => IsSmallCave(n)))
+                {
+                    if(!visitedSmallCave.Add(node))
+                    {
+                        aNodeHasBeenVisitedTwice = true;
+                    }
+                }
+
+                if (aNodeHasBeenVisitedTwice)
+                {
+                    return !visitedSmallCave.Contains(smallCave);
+                }
+                return true;
+            }
+        }
+
+
+        protected override long Part1SampleResult => 10;
+        protected override long SolvePart1(string[] input)
+        {
+            return CaveMap.ReadFromFile(input).CountPathsWithoutVisitingSmallCaveTwice();
+        }
+
+        protected override long Part2SampleResult => 36;
+        protected override long SolvePart2(string[] input)
+        {
+            var caveMap = CaveMap.ReadFromFile(input);
+
+            return caveMap.CountPathVisitingASingleSmallCaveTwice();
+        }
+    }
+}
